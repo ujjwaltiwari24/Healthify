ef8c8b9f22b0f6bf2f6999e20a90ae2e3b7685ce
Ujjwal Tiwari <ujjwal.business2024@gmail.com>
2025-03-11 21:21:32 +0530

    sliding banners added

--- Healthify/scripts.js
+++ Healthify/scripts.js
@@ -1,3 +1,25 @@
 document.getElementById('menu-icon').addEventListener('click', () => {
     document.getElementById('nav-links').classList.toggle('show');
 });
+
+
+/*Scrollable banners*/
+
+let currentIndex = 0;
+
+function moveSlide(direction) {
+  const slides = document.querySelectorAll('.banner-slide img');
+  const totalSlides = slides.length;
+
+  currentIndex += direction;
+
+  if (currentIndex < 0) {
+    currentIndex = totalSlides - 1;
+  } else if (currentIndex >= totalSlides) {
+    currentIndex = 0;
+  }
+
+  const slideWidth = slides[0].clientWidth;
+  const newTransformValue = -currentIndex * slideWidth;
+  document.querySelector('.banner-slide').style.transform = `translateX(${newTransformValue}px)`;
+}
